6cbcbdc0c57b9d975f334f9b5278f3639b9fc2ad
Nico-Capy <nicolacorradini97@yahoo.com>
2023-04-24 14:39:13 +0200

    Button + home page

---
+++ src/components/explore.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="rounded-none m-1 p-1 bg-transparent hover:bg-white text-white hover:text-black flex items-center drop-shadow-xl" @click="goToGalleries">
+      <span>Explore Galleries</span>
+      <span class="ml-2">&#10132;</span>
+    </button>
+  </template>
+  <script>
+  export default {
+    name: 'ExploreButton',
+    methods: {
+      goToGalleries() {
+        this.$router.push('/galleries');
+      }
+    }
+  }
+  </script>
+  <style>
+  .drop-shadow-xl {
+    text-shadow: 0px 0px 10px rgba(255, 255, 255, 0.45), 0px 0px 10px rgba(255, 255, 255, 0.45);
+  }
+  </style> 
--- src/view/home.vue
+++ src/view/home.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="flex flex-row justify-center h-screen" role="banner" aria-label="Header">
-    <h1 class="text-4xl font-bold text-white" role="heading" aria-level="1">Nicola Corradini</h1>
-    <h2 class="text-2xl text-gray-300 indent-2" role="heading" aria-level="2">| Photography</h2>
+  <div class="flex flex-col items-center drop-shadow-xl" role="banner" aria-label="Header">
+    <div class="flex flex-row sm:flex-col md:flex-col lg:flex-row xl:flex-row 2xl:flex-row items-center">
+      <h1 class="text-2xl sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-white" role="heading" aria-level="1">Nicola Corradini</h1>
+      <h2 class="text-lg text-gray-300" role="heading" aria-level="2">| Photography</h2>
+    </div>
+    <div class="flex items-center justify-center mt-6">
+      <explore-button />
+    </div>
   </div>
-  <div class="flex flex-col items-center" role="img" aria-label="Pendjari Elephant">
-    <img src="../assets/preview/preview3.jpg" alt="Pendjari Elephant" class="w-2/3 h-full"/>
-  </div>
+  <div class="w-11/12 m-4 flex items-center justify-center drop-shadow-lg">
+      <img src="../assets/preview/preview3.jpg" alt="Pendjari elephant" class="h-2/3 sm:h-1/3 md:h-1/3">
+    </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import ExploreButton from '../components/explore.vue';
+
+export default {
+  components: {
+    ExploreButton,
+  },
+};
+</script>
 
 <style>
   body {
