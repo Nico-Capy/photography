7f34ed04ae1b88bb1e54dca351a082c7f6da8bb9
Nico-Capy <nicolacorradini97@yahoo.com>
2023-05-05 16:30:47 +0200

    Urkelgrau

--- src/view/about.vue
+++ src/view/about.vue
@@ -1,37 +1,38 @@
 <template>
-    <div class="flex flex-col items-center justify-center">
-      <h2 class="text-3xl text-white drop-shadow-xl m-6 mb-0">About</h2>
-    </div>
-    <div class="flex flex-row text-white m-0 p-4">
-        <h3 class="text-3xl indent-14 drop-shadow-xl">Hi, I'm</h3>
-        <h3 class="text-3xl text-black bg-white px-3 ml-2 shadow">Nico</h3>
-    </div>
+  <div class="flex flex-col items-center justify-center">
+    <h2 class="text-3xl text-white drop-shadow-xl m-6 mb-0">About</h2>
+  </div>
+  <div class="flex flex-row text-white m-6 w-4/5 items-center justify-center">
+    <h3 class="text-3xl indent-14 drop-shadow-xl">Hi, I'm</h3>
+    <h3 class="text-3xl text-black bg-white px-3 ml-2 shadow">Nico</h3>
+  </div>
 
-    <div class="flex flex-row">
-      <div class="flex flex-col text-white drop-shadow-xl m-6 mt-0">
-        <p class="text-xl p-4 indent-4">I'm a developer, photographer, drone pilot, videographer, and brewer. Sometimes, I merge these pursuits. Behold, an online photo gallery!</p>
-            <br/>
-            <br/>
-        <p class="text-xl p-4 indent-4">Photography was not my calling. I stumbled into it as a means to escape the tedium of school. But I realized that the answer was to innovate. And experiment, I did. From drones to pinhole lenses, from infrared to analog, from street photography to studio portraits — I explored it all. Suffice to say, I reveled in the thrill of discovery.</p>
-    </div>
-    </div>
+  <div class="mx-auto flex flex-row w-4/5 justify-center align-center">
     
-    <div class="flex justify-center align-center">
-      <img src="/DJI_0970.jpg" alt="Admiral Nico, flying his drone" class="w-4/5 justify-center align-center m-2" />
+    <div class="flex flex-col text-white drop-shadow-xl m-6">
+      <p class="text-xl p-4 indent-4">I'm a developer, photographer, drone pilot, videographer, and brewer. Sometimes, I merge these pursuits. Behold, an online photo gallery!</p>
+      <br />
+      <br />
+      <p class="text-xl p-4 indent-4">Photography was not my calling. I stumbled into it as a means to escape the tedium of school. But I realized that the answer was to innovate. And experiment, I did. From drones to pinhole lenses, from infrared to analog, from street photography to studio portraits — I explored it all. Suffice to say, I reveled in the thrill of discovery.</p>
     </div>
+  </div>
+
+  <div class="flex justify-center align-center">
+    <img src="/DJI_0970.jpg" alt="Admiral Nico, flying his drone" class="w-3/5 justify-center align-center m-2" />
+  </div>
 </template>
 
 <style>
- h2, p {
-   font-family: 'Avenir', sans-serif;
- }
+h2,
+p {
+  font-family: 'Avenir', sans-serif;
+}
 
-  .drop-shadow-lg {
-      text-shadow: 0px 0px 6px rgba(255, 255, 255, 0.4),
-                   0px 0px 6px rgba(255, 255, 255, 0.4);
-  }
+.drop-shadow-lg {
+  text-shadow: 0px 0px 6px rgba(255, 255, 255, 0.4), 0px 0px 6px rgba(255, 255, 255, 0.4);
+}
 
-  .shadow{
-    text-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
-  }
+.shadow {
+  text-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
+}
 </style>
