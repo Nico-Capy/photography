30b3ee3617b10b67f96ac4d1eada07fd3e66927d
Nico-Capy <nicolacorradini97@yahoo.com>
2023-05-07 19:49:27 +0200

    Added components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,21 @@
 <script lang="ts">
   import { defineComponent } from 'vue'
   import Navbar from './components/navbar.vue'
+  import DroneButton from "../src/components/dronebutton.vue"
   import Footer from '../src/components/footer.vue'
   import Home from "../src/view/home.vue"
   import About from "../src/view/about.vue"
+  import Drone from '../src/view/drone.vue'
+  import Portraits from "../src/view/studio.vue"
+  import Nautre from "../src/view/animals.vue"
+  import Analog from '../src/view/analog.vue'
+  import Galleries from "../src/view/galleries.vue"
+  import Infrared from '../src/view/infrared.vue'
+  import NotFound from "../src/view/notfound.vue"
+  import Others from "../src/view/others.vue"
+  import Street from '../src/view/street.vue'
+  import Pinhole from "../src/view/pinhole.vue"
+  import Random from "../src/view/random.vue"
 
   export default defineComponent({
     components: {
@@ -17,6 +29,18 @@
       Footer,
       Home,
       About,
+      Drone,
+      Portraits,
+      Nautre,
+      Analog,
+      Galleries,
+      Infrared,
+      NotFound,
+      Others,
+      Street,
+      Pinhole,
+      Random,
+      DroneButton
     },
   })
 </script>
