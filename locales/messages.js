f7886512004078111f7ebb1d280add2ec132e60e
Nico Corradini <81596462+Nico-Capy@users.noreply.github.com>
2023-05-12 18:28:46 +0200

    Removed Latin from messages.js

--- locales/messages.js
+++ locales/messages.js
@@ -1,13 +1,13 @@
 import en from './en.json';
 import fr from './fr.json';
 import it from './it.json';
-import la from "./la.json";
+// import la from "./la.json";
 
 const messages = {
   en,
   fr,
   it,
-  la,
+//  la,
 };
 
 export default messages;
