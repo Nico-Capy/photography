7497c8c248c72287f68c22f7487e80386cce02f5
Nico Corradini <81596462+Nico-Capy@users.noreply.github.com>
2023-05-12 18:26:56 +0200

    Removed Latin from LanguageSwitcher.vue

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -11,7 +11,11 @@
 export default {
   data() {
     return {
-      supportedLanguages: ['en', 'fr', 'it', 'la'],
+      supportedLanguages: ['en',
+														'fr',
+														'it',
+												// 'la'
+														],
       selectedLanguage: this.$i18n.locale,
     };
   },
