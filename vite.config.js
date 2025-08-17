16aac006d579cd2705976dce7a54027a8396a9c9
Nico-Capy <nicolacorradini97@yahoo.com>
2025-08-18 00:46:51 +0200

    Updated NPM + cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import image from "vite-image";
+import { imagetools } from "vite-imagetools";
+import Fonts from "unplugin-fonts/vite";
 
 export default defineConfig({
-  plugins: [vue(), image()],
+  plugins: [
+    vue(),
+    imagetools(),
+    Fonts({
+      google: {
+        families: [
+          {
+            name: "Roboto",
+            styles: "wght@400;700",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3020,
   },
