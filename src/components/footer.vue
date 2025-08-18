697f7fff3260934eee6a5ce4291cab5e4679773d
Nico-Capy <nicolacorradini97@yahoo.com>
2025-08-18 12:40:12 +0200

    Cleaned un a file

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,16 +1,17 @@
+vue
 <template>
   <footer
-    class="flex flex-col lg:flex-row xl:flex-row 2xl:flew-row p-0 bg-transparent text-center text-sm w-fit drop-shadow-xl sm:fa-align-center mt-3"
+    class="flex flex-col lg:flex-row xl:flex-row 2xl:flex-row p-0 bg-transparent text-center text-sm w-fit drop-shadow-xl sm:fa-align-center mt-3"
     :style="{ backgroundColor: backgroundColor }"
   >
-   <a
-  href="https://youtu.be/ZrF7MEWojQ0?si=5krp37w1AMIZ255t"
-  target="_blank"
-  rel="noopener noreferrer"
-  class="px-4 py-1 text-white no-underline hover:opacity-80 transition-opacity"
->
-  © Nicola Corradini | <span v-html="formattedDate"></span>
-</a>
+    <a
+      href="https://youtu.be/ZrF7MEWojQ0?si=5krp37w1AMIZ255t"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="px-4 py-1 text-white no-underline hover:opacity-80 transition-opacity"
+    >
+      © Nicola Corradini | <span v-html="formattedDate"></span>
+    </a>
     <font-awesome-icon
       :icon="faJediOrder"
       style="color: white; cursor: pointer; height: 3vh"
@@ -18,12 +19,10 @@
     />
   </footer>
 </template>
-
 <script lang="ts">
 import { defineComponent } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faJediOrder } from "@fortawesome/free-brands-svg-icons";
-
 export default defineComponent({
   name: "Footer",
   components: {
@@ -31,7 +30,6 @@ export default defineComponent({
   },
   data() {
     return {
-      currentDate: this.getCurrentDate(),
       backgroundColor: "transparent",
     };
   },
@@ -43,7 +41,6 @@ export default defineComponent({
       const date = new Date();
       const day = date.getDate();
       let daySuffix: string;
-
       if (day === 1 || day === 21 || day === 31) {
         daySuffix = "st";
       } else if (day === 2 || day === 22) {
@@ -53,7 +50,6 @@ export default defineComponent({
       } else {
         daySuffix = "th";
       }
-
       const options: Intl.DateTimeFormatOptions = {
         month: "long",
         day: "numeric",
@@ -62,7 +58,6 @@ export default defineComponent({
       const formatter = new Intl.DateTimeFormat("en-US", options);
       const formattedDate: string = formatter.format(date);
       const formattedDay = `${day}<sup>${daySuffix}</sup>`;
-
       return formattedDate
         .replace(/,/g, "")
         .replace(day.toString(), formattedDay);
@@ -72,13 +67,12 @@ export default defineComponent({
     faJediOrder() {
       return faJediOrder;
     },
-    formattedDate() {
+    formattedDate(): string {
       return this.getCurrentDate();
-    },
-  },
+    }
+  }
 });
 </script>
-
 <style scoped>
 footer {
   position: fixed;
